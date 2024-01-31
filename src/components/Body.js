5f149008720c2c5fb712d61115ac4f67563a2b65
rajeshwari2538 <rajeshwari2538@gmail.com>
2024-01-31 14:47:57 +0530

    implemented watch movie component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Browse from './Browse'
 import Login from './Login'
+import WatchMovie from './WatchMovie'
 import Header from './Header'
 import ErrorPage from './ErrorPage'
 
@@ -14,7 +15,8 @@ const Body = () => {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/browse" element={<Browse/>}/>
-            
+            <Route path='/movie/:movieId' element={<WatchMovie />}/>
+
             <Route path='*' element={<Navigate to="/error" />} />
             <Route path='/error' element={<ErrorPage />} />
           </Routes>
